8edc1437b6bd77fd3170818533e04d8162566977
clear <oopaletijohnson@gmail.com>
2026-04-21 04:02:16 +0100

    TypeScript Specific ES Module Syntax

---
+++ hello.ts
@@ -0,0 +1,4 @@
+// A file can declare a `main export` via export default:
+export default function helloWorld() {
+  console.log("Hello world");
+}
--- modules.ts
+++ modules.ts
@@ -19,6 +19,58 @@ in the shared global scope,
 If you have a file that doesn’t currently have any imports or exports, 
 but you want to be treated as a module, add the line:
 */
-export { }
+export {};
 
 /* ----------- Modules in TypeScript ---------- */
+/* 🧠 There are three main things to consider when writing module-based code:
+👉 Syntax: What syntax do I want to use to import and export things?
+👉 Module Resolution: What is the relationship between module names (or paths) 
+and files on disk?
+👉 Module Output Target: What should my emitted JavaScript module look like? */
+
+// ES Module Syntax:
+// A file can declare a `main export` via export default:
+// NOTE:: A code is written in hello.ts file
+`export default function helloWorld() {
+  console.log("Hello world");
+}`;
+
+// This is then imported from `hello.ts` via:
+import helloWorld from "./hello";
+helloWorld();
+
+/* In addition to the default export, you can have more than one export of 
+variables and functions via the `export` by omitting default: */
+// NOTE:: THE EXPORT CODE IS IN @filename: maths.ts
+
+// These can be used in another file via the import syntax:
+import { pi, phi, absolute } from "./math";
+console.log(pi);
+const absPhi = absolute(phi);
+
+/* ----------- Additional Import Syntax ---------- */
+// An import can be renamed using a format like import {old as new}:
+import { pi as π } from "./math";
+console.log(π);
+
+// You can mix and match the above syntax into a single import:
+import DefaultRandomNumberGenerator, { pi as PI } from "./math";
+DefaultRandomNumberGenerator;
+console.log(PI);
+
+/* You can take all of the exported objects and put them into a 
+single namespace using * as name: */
+import * as math from "./math";
+console.log(math.pi);
+const positivePhi = math.absolute(math.phi);
+
+/* You can import a file and not include any variables into your current 
+module via import "./file": 
+In this case, the import does nothing. However, all of the code in maths.ts 
+was evaluated, which could trigger side-effects which affect other objects. */
+import "./math";
+console.log("3.14");
+
+/* ----------- TypeScript Specific ES Module Syntax ---------- */
+/* Types can be exported and imported using the same syntax 
+as JavaScript values: */
