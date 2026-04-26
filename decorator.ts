7200f5844e0bf6d21457773a7c7849cde43b4963
clear <oopaletijohnson@gmail.com>
2026-04-26 23:43:27 +0100

    ...

--- decorator.ts
+++ decorator.ts
@@ -1,2 +1,9 @@
 /* Decorators
-A Decorator is a special kind of declaration that can be attached to a class declaration, method, accessor, property, or parameter. Decorators use the form @expression, where expression must evaluate to a function that will be called at runtime with information about the decorated declaration. */
+A Decorator is a special kind of declaration that can be attached 
+to a class declaration, method, accessor, property, or parameter. 
+Decorators use the form @expression, where expression must evaluate 
+to a function that will be called at runtime with information about 
+the decorated declaration. */
+function sealed(target) {
+  // do something with 'target' ...
+}
