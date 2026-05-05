3248d3494d6563f322ee718425f14bb77b5731fa
clear <oopaletijohnson@gmail.com>
2026-05-05 14:35:21 +0100

    tripple-slash directives

---
+++ trippleSlash.ts
@@ -0,0 +1,28 @@
+/* ------------------ Triple-Slash Directives --------------- */
+/* Triple-slash directives are single-line comments containing a single XML 
+tag. The contents of the comment are used as compiler directives. 
+>>Triple-slash directives are only valid at the top of their containing file. 
+A triple-slash directive can only be preceded by single or multi-line 
+comments, including other triple-slash directives. */
+
+// This `directive` serves as a declaration of dependency between files.
+/// <reference path="learn.ts" />
+
+/* This directive declares a dependency on a package.
+/// <reference types="..." /> */
+
+/* This directive allows a file to explicitly include an existing 
+built -in lib file. */
+/// <reference lib="es2017.string" />
+
+/* This directive marks a file as a default library. You will see this 
+comment at the top of lib.d.ts and its different variants. */
+/* /// <reference no-default-lib="true" /> */
+
+
+/* By default AMD modules are generated anonymous. This can lead to problems 
+when other tools are used to process the resulting modules, such as bundlers 
+The amd-module directive allows passing an optional module name to the 
+compiler: */
+/// <amd-module name="NamedModule" />
+export class C { };
