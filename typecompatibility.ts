288ede5b0a0ccd52b16f2a2c7f1a60e69ac23438
clear <oopaletijohnson@gmail.com>
2026-05-09 22:55:45 +0100

    ...

--- typecompatibility.ts
+++ typecompatibility.ts
@@ -6,3 +6,31 @@ items.forEach((item, index, array) => {
 
 // Should be OK!
 items.forEach((item) => console.log(item));
+
+/* `SETTIMEOUT` - RUNS A FUNCTION ONCE AFTER A SPECIFIED DELAY 
+   `SETINTERVAL` - RUNS A FUNCTION REPEATEDLY AT A FIXED INTERVALS */
+setTimeout(() => { console.log("Runs Once After 5 Seconds") }, 5000)
+// setInterval(()=> console.log("Runs repeatedly after 1 second"), 1000)
+
+for (var i = 0; i < 10; i++) {
+    // capture the current state of 'i'
+    // by invoking a function with its current value
+    (function (x) {
+        setTimeout(function() {console.log(x)}, 100 * x)
+    })(i);
+}
+
+let fa = 400;
+function foo() {
+    let fa = 800
+    console.log(fa)
+    return A1
+}
+
+let A1 = 2
+foo()
+
+let xf = 10;
+// let xf = 20;
+console.log(fa);
+// 
